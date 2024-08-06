763417c7e31d5d5133b998b7aa22e0b078818062
Swamstick911 <163744064+Swamstick911@users.noreply.github.com>
2024-08-06 05:38:36 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+class EvenOdd
+{
+    public static void main(int number)
+    {
+        System.out.println("Entered number is: "+ number);
+        if(number%2==0)
+        {
+            System.out.println("Entered number is Even!");
+        }
+        else
+        {
+            System.out.println("Entered number is odd!");
+        }
+    }
+}
