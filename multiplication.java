3e56d46b51a87b40828ad9ec7bd2fa00b177d818
Swamstick911 <163744064+Swamstick911@users.noreply.github.com>
2024-08-06 05:37:31 +0530

    Create multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,9 @@
+class multiplication
+{
+    public static void main(int num)
+    {
+        System.out.println("Entered number is: "+ num);
+        int product = num*num;
+        System.out.println("The square of the number is: "+ product);
+    }
+}
