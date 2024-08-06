c8e1f4e11e0d6fdac94efe9fe562c11df62aec1d
Swamstick911 <163744064+Swamstick911@users.noreply.github.com>
2024-08-06 05:57:57 +0530

    Create language.java

---
+++ language.java
@@ -0,0 +1,11 @@
+class language
+{
+    public static void main(String[]args)
+    {
+        String lang = "Java";
+        if (lang == "Java")
+        {
+            System.out.println("Object Oriented Programming language");
+        }
+    }
+}
