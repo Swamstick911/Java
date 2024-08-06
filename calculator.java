a4e7bcb45db9aba7de0e806a43c7fb4e22f37a95
Swamstick911 <163744064+Swamstick911@users.noreply.github.com>
2024-08-06 05:58:47 +0530

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,16 @@
+class calculator
+{
+    public static void main(int num1, int num2)
+    {
+        int sum = num1 + num2;
+        int product = num1 * num2;
+        int division = num1 / num2;
+        int remainder = num1 % num2;
+        int subtract = num1 - num2;
+        System.out.println("The sum of the numbers is: "+ sum);
+        System.out.println("The product of the numbers is: "+ product);
+        System.out.println("The quotient of the numbers is: "+ division);
+        System.out.println("The remainder of the numbers is: "+ remainder);
+        System.out.println("The subtraction of the numbers is: "+ subtract);
+    }
+}
